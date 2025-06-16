ae42100d4a0b22b1b8a85525a85494927f2e5751
Erohsik-0 <kishorekishore9725@gmail.com>
2025-06-16 15:04:16 +0530

    Implementing middleware model for using c#

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
+using System.Collections.Generic;
 using StyleSphere.Models;
+using System.Text.Json;
+
 
 namespace StyleSphere.Controllers
 {
@@ -16,24 +17,66 @@ namespace StyleSphere.Controllers
 
         public IActionResult Index()
         {
+
             return View();
         }
 
         public IActionResult Details(int id)
         {
-            return View();
+
+            var products = LoadProducts();
+            if (products == null)
+            {
+                return NotFound("Product data not found");
+            }
+
+            //Using a dictionary for fast lookup by ID
+            var productDictionary = new Dictionary<int, Products>();
+            foreach (var p in products)
+            {
+                productDictionary[p.id] = p;
+            }
+
+            if (!productDictionary.TryGetValue(id, out var product))
+            {
+                return NotFound($"Product with ID {id} not found...");
+            }
+
+            return View(product); //Passing product model to the view
         }
 
         [HttpGet]
         public IActionResult GetProducts()
         {
+            //string jsonPath = Path.Combine(_env.WebRootPath, "Data", "products.json");
+
+            //if (!System.IO.File.Exists(jsonPath))
+            //    return NotFound("products.json not found.");
+
+            //string json = System.IO.File.ReadAllText(jsonPath);
+            //return Content(json, "application/json");
+
+            var products = LoadProducts();
+            if (products == null)
+            {
+                return NotFound("Products.json not found");
+            }
+
+            return Json(products); // Returning as JSON using C# List<Product> directly
+        }
+
+        private List<Products>? LoadProducts()
+        {
             string jsonPath = Path.Combine(_env.WebRootPath, "Data", "products.json");
 
-            if (!System.IO.File.Exists(jsonPath))
-                return NotFound("products.json not found.");
+            if ( !System.IO.File.Exists(jsonPath))
+            {
+                return null;
+            }
 
             string json = System.IO.File.ReadAllText(jsonPath);
-            return Content(json, "application/json");
+            return JsonSerializer.Deserialize<List<Products>>(json);
         }
+
     }
 }
